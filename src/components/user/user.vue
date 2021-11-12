f3fabcc03fde00e031d1213f882b610cf754b989
Nikolai Kolotilov <x08042@gamail.com>
2021-11-12 22:25:06 +0500

    make user

---
+++ src/components/user/user.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="user">
+        <img class="user__img" src="https://picsum.photos/200" alt="">
+         <div class="user__name">User Name</div>
+    </div>
+</template>
+<style src="./user.scss" lang="scss" scoped></style>
